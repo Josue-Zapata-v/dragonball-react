5e92bda973bf7adb98f88aa6efb7ab081dbb95a7
Josue-Zapata-v <zjosue775@gmail.com>
2025-11-05 22:05:09 -0500

    feat: agrega pagina principal, importa HeroSection y CharacterList

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../components/home/HeroSection';
+import CharacterList from '../components/home/CharacterList';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <CharacterList />
+    </>
+  );
+};
+
+export default HomePage;
