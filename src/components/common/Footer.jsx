9662544a95d84830eb2bef4b680e5b14180befb9
Josue-Zapata-v <zjosue775@gmail.com>
2025-11-05 21:58:39 -0500

    feat: agrega footer global de la app

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 mt-auto">
+      &copy; 2025 Dragon Ball Explorer
+    </footer>
+  );
+};
+
+export default Footer;
