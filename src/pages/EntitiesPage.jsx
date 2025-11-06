4485a14ebb3b691245d0d34b51a3a8f7303606f8
Josue-Zapata-v <zjosue775@gmail.com>
2025-11-05 22:06:24 -0500

    feat: agrega pagina de entidades, importa EntityList

---
+++ src/pages/EntitiesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EntityList from '../components/entities/EntityList';
+
+const EntitiesPage = () => {
+  return (
+    <>
+      <h2 className="mb-4">Lista de Entidades</h2>
+      <EntityList />
+    </>
+  );
+};
+
+export default EntitiesPage;
