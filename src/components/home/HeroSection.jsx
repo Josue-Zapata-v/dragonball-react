913959391331c85ebad4eee8797ac0649906fc98
Josue-Zapata-v <zjosue775@gmail.com>
2025-11-05 21:56:43 -0500

    feat: define las rutas y el layout general-app.jsx

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className="text-center my-5">
+      <h1>Dragon Ball Explorer</h1>
+      <p>Explora los personajes del universo Dragon Ball de manera sencilla.</p>
+    </div>
+  );
+};
+
+export default HeroSection;
